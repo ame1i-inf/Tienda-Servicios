009a8237e976eb00ba02cf6e72a57ade384235be
maric <maric@AME>
2026-03-26 13:46:15 -0600

    primeros archivos

---
+++ Tienda Servicios/Modelo/AutorLibro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tienda_Servicios.Modelo
+{
+    public class Autor_Libro
+    {
+        public int Autor_LibroId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public ICollection<GradoAcademico> ListaGradoAcademico { get; set; }
+        public string AutorLibroGuid { get; set; }
+
+    }
+}
---
+++ Tienda Servicios/Modelo/GradoAcademico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tienda_Servicios.Modelo
+{
+    public class GradoAcademico
+    {
+        public int GradoAcademicoId { get; set; }
+        public string Nombre { get; set; }
+        public string CentroAcademico { get; set; }
+        public DateTime? FechaGrado { get; set; }
+        public int Autor_LibroId { get; set; }
+        public Autor_Libro Autor_Libro { get; set; }
+        public string GradoAcademicoGuid { get; set; }
+    }
+}
